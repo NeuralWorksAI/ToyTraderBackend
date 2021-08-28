50faa75f58c14fcc8fd90677c82df44ff1bcca78
arjun006 <arjun.devakumar66@gmail.com>
2021-08-28 03:53:41 -0400

    dotenv

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,8 @@
+const dotenv = requires("dotenv")
+dotenv.config();
+const DB_PASS = process.env.DB_PASS;
 module.exports = {
-    mongoURI: "mongodb+srv://dbUser:LSwNe08yd9QqVDYq@toytrader.cqqqn.mongodb.net/toytrader?retryWrites=true&w=majority",
+    mongoURI: `mongodb+srv://dbUser:${DB_PASS}@toytrader.cqqqn.mongodb.net/toytrader?retryWrites=true&w=majority`,
     secretOrKey: "secret"
     
   };
