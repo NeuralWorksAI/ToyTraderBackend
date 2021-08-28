17776a1b109e38b562fcb52f57c5f3d67f79dfbb
arjun006 <arjun.devakumar66@gmail.com>
2021-08-28 02:41:44 -0400

    user auth

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    mongoURI: "mongodb+srv://dbUser:LSwNe08yd9QqVDYq@toytrader.cqqqn.mongodb.net/toytrader?retryWrites=true&w=majority",
+    secretOrKey: "secret"
+    
+  };
--- server.js
+++ server.js
@@ -1,15 +1,41 @@
-const express = require('express');
-const router = express.Router();
-const path = require("path");
-const dotenv = require('dotenv')
-dotenv.config();
-const bodyParser = require('body-parser');
-const cors = require('cors');
-//Express Connection
-const app = express();
-var HTTP_PORT = process.env.PORT || 5000;
-const urlencodedParser = bodyParser.urlencoded({ extended: false });
-app.use(bodyParser.json());
-app.use(express.json());
-app.use(cors());
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const passport = require("passport");
 
+const users = require("./routes/api/users");
+
+const app = express();
+
+// Bodyparser middleware
+app.use(
+  bodyParser.urlencoded({
+    extended: false
+  })
+);
+app.use(bodyParser.json());
+
+// DB Config
+const db = require("./config/keys").mongoURI;
+
+// Connect to MongoDB
+mongoose
+  .connect(
+    db,
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log("MongoDB successfully connected"))
+  .catch(err => console.log(err));
+
+// Passport middleware
+app.use(passport.initialize());
+
+// Passport config
+require("./config/passport")(passport);
+
+// Routes
+app.use("/api/users", users);
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server up and running on port ${port} !`));
